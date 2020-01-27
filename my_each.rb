c18ee4bf2662fd7b1071cf318d43bbd1501e893f
Samantha Jordan <shinyguns@gmail.com>
2020-01-27 20:49:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,6 +2,7 @@ def my_each(array) # put argument(s) here
   i = 0 # start with 0 for loop counter
   
   while i < array.length #runs while there is .length on array
+  yield(array[i]) #yield each one of the array
    i += 1 #add one to counter each loop
 end
   
