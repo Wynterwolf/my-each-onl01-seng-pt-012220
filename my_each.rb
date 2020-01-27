3d585576232c5d4f2dce7008b0952d936cd51891
Samantha Jordan <shinyguns@gmail.com>
2020-01-27 20:51:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,14 @@
 def my_each(array) # put argument(s) here
+  if block_given? # in case no block is block_given
   i = 0 # start with 0 for loop counter
   
   while i < array.length #runs while there is .length on array
   yield(array[i]) #yield each one of the array
    i += 1 #add one to counter each loop
 end
-  
+  array
+else
+  puts "No block was given!"
+end
   
 end
